d90ea3f74ef5d293542c07ac74e960e38fe66406
Chargonium <svenvandoorn@icloud.com>
2024-08-17 20:51:35 +0200

    Initial Commit

---
+++ src/main/kotlin/com/chargonium/ccclient/SodiumExtrasDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.chargonium.ccclient
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SodiumExtrasDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
