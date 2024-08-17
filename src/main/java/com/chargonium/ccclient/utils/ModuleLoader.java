7b982ab5e260e5558cf08ef937c75f1219f37ef2
Chargonium <svenvandoorn@icloud.com>
2024-08-18 01:15:27 +0200

    ModuleLoader + AimAssist improvements

--- src/main/java/com/chargonium/ccclient/utils/ModuleLoader.java
+++ src/main/java/com/chargonium/ccclient/utils/ModuleLoader.java
@@ -15,4 +15,16 @@ public class ModuleLoader {
         return true;
     }
 
+
+    public boolean registerModule(Object instance, String MainMethodName) {
+        try {
+            Method method = instance.getClass().getMethod(MainMethodName);
+
+            method.invoke(instance);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
 }
